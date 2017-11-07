4e700cba796f2dd617e3262ad0ea316ecd30c972
Frank Rogneslien <frank.rogneslien@ice.net>
2017-11-07 11:43:44 +0000

    refactor: indentation, reorder imports

--- web/src/index.js
+++ web/src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './css/bootstrap.min.css';
-import './css/icons.css';
-import './css/index.css';
-import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
 import { createStore, applyMiddleware, compose } from 'redux';
-import reducer from './reducers/root';
 import { Provider } from 'react-redux';
 import { Route, BrowserRouter } from 'react-router-dom';
 import thunkMiddleware from 'redux-thunk';
+import registerServiceWorker from './registerServiceWorker';
+import reducer from './reducers/root';
+import App from './components/App';
+import './css/bootstrap.min.css';
+import './css/icons.css';
+import './css/index.css';
+
+
 
 const logger = store => next => action => {
     console.group(action.type);
