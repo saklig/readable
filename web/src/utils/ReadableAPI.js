c66dcb8c0d9c3104b375e69ec0a3cf355c6a1a0b
Frank Rogneslien <frank.rogneslien@ice.net>
2017-11-07 11:44:23 +0000

    refactor: consistency, readability

--- web/src/utils/ReadableAPI.js
+++ web/src/utils/ReadableAPI.js
@@ -6,32 +6,35 @@ const headers = {
     'Content-Type': 'application/json'
 };
 
-export const fetchPosts = () => fetch(`${api}/posts`, { headers });
+export const fetchPosts = () =>
+    fetch(`${api}/posts`, {
+        headers
+    });
 
-export const fetchPost = (id) => fetch(`${api}/posts/${id}`, { headers });
+export const fetchPost = (id) =>
+    fetch(`${api}/posts/${id}`, {
+        headers
+    });
 
-export const addPost = (post) => {
-    return fetch(`${api}/posts`, {
+export const addPost = (post) =>
+    fetch(`${api}/posts`, {
         headers,
         method: 'post',
         body: JSON.stringify(post)
     });
-};
 
-export const removePost = (id) => {
+export const removePost = (id) =>
     fetch(`${api}/posts/${id}`, {
         headers,
         method: 'delete'
     });
-};
 
-export const updatePost = (post) => {
+export const updatePost = (post) =>
     fetch(`${api}/posts/${post.id}`, {
         headers,
         method: 'put',
         body: JSON.stringify(post)
     });
-};
 
 export const upVotePost = (id) =>
     fetch(`${api}/posts/${id}`, {
@@ -47,45 +50,47 @@ export const downVotePost = (id) =>
         body: JSON.stringify({option: 'downVote'})
     });
 
-export const fetchComments = (postId) => fetch(`${api}/posts/${postId}/comments`, { headers });
+export const fetchComments = (postId) =>
+    fetch(`${api}/posts/${postId}/comments`, {
+        headers
+    });
 
-export const fetchComment = (commentId) => fetch(`${api}/comments/${commentId}`, { headers });
+export const fetchComment = (commentId) =>
+    fetch(`${api}/comments/${commentId}`, {
+        headers
+    });
 
-export const addComment = (comment) => {
+export const addComment = (comment) =>
     fetch(`${api}/comments`, {
         headers,
         method: 'post',
         body: JSON.stringify(comment)
     });
-};
 
-export const removeComment = (id) => {
+export const removeComment = (id) =>
     fetch(`${api}/comments/${id}`, {
         headers,
         method: 'delete'
     });
-};
 
-export const updateComment = (comment) => {
+export const updateComment = (comment) =>
     fetch(`${api}/comments/${comment.id}`, {
         headers,
         method: 'put',
         body: JSON.stringify(comment)
     });
-};
 
-export const upVoteComment = (id) => {
+
+export const upVoteComment = (id) =>
     fetch(`${api}/comments/${id}`, {
         headers,
         method: 'post',
         body: JSON.stringify({option: 'upVote'})
     });
-};
 
-export const downVoteComment = (id) => {
+export const downVoteComment = (id) =>
     fetch(`${api}/comments/${id}`, {
         headers,
         method: 'post',
         body: JSON.stringify({option: 'downVote'})
     });
-};
