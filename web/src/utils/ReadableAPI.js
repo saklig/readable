350ababe9035f3cce44044aa6065f3d0e8fdef50
Frank Rogneslien <frank.rogneslien@ice.net>
2017-11-07 08:33:34 +0000

    feat: implemented more functions in readableapi

--- web/src/utils/ReadableAPI.js
+++ web/src/utils/ReadableAPI.js
@@ -8,6 +8,8 @@ const headers = {
 
 export const fetchPosts = () => fetch(`${api}/posts`, { headers });
 
+export const fetchPost = (id) => fetch(`${api}/posts/${id}`, { headers });
+
 export const addPost = (post) => {
     return fetch(`${api}/posts`, {
         headers,
@@ -23,16 +25,67 @@ export const removePost = (id) => {
     });
 };
 
+export const updatePost = (post) => {
+    fetch(`${api}/posts/${post.id}`, {
+        headers,
+        method: 'put',
+        body: JSON.stringify(post)
+    });
+};
+
 export const upVotePost = (id) =>
-fetch(`${api}/posts/${id}`, {
-    headers,
-    method: 'post',
-    body: JSON.stringify({option: 'upVote'})
-});
+    fetch(`${api}/posts/${id}`, {
+        headers,
+        method: 'post',
+        body: JSON.stringify({option: 'upVote'})
+    });
 
 export const downVotePost = (id) =>
-fetch(`${api}/posts/${id}`, {
-    headers,
-    method: 'post',
-    body: JSON.stringify({option: 'downVote'})
-}); 
+    fetch(`${api}/posts/${id}`, {
+        headers,
+        method: 'post',
+        body: JSON.stringify({option: 'downVote'})
+    });
+
+export const fetchComments = (postId) => fetch(`${api}/posts/${postId}/comments`, { headers });
+
+export const fetchComment = (commentId) => fetch(`${api}/comments/${commentId}`, { headers });
+
+export const addComment = (comment) => {
+    fetch(`${api}/comments`, {
+        headers,
+        method: 'post',
+        body: JSON.stringify(comment)
+    });
+};
+
+export const removeComment = (id) => {
+    fetch(`${api}/comments/${id}`, {
+        headers,
+        method: 'delete'
+    });
+};
+
+export const updateComment = (comment) => {
+    fetch(`${api}/comments/${comment.id}`, {
+        headers,
+        method: 'put',
+        body: JSON.stringify(comment)
+    });
+};
+
+export const upVoteComment = (id) => {
+    fetch(`${api}/comments/${id}`, {
+        headers,
+        method: 'post',
+        body: JSON.stringify({option: 'upVote'})
+    });
+};
+
+export const downVoteComment = (id) => {
+    fetch(`${api}/comments/${id}`, {
+        headers,
+        method: 'post',
+        body: JSON.stringify({option: 'downVote'})
+    });
+};
